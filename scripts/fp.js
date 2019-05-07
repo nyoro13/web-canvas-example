660a22f32ea8c7f163d3af8cf4ef9aac8e926102
nyoro13 <konnichiwa13@gmail.com>
2019-05-07 15:01:26 +0900

    特徴点検出

--- scripts/fp.js
+++ scripts/fp.js
@@ -3,12 +3,13 @@
 class OpenCVVideo {
   constructor(canvasId, videoElement) {
     this.fps = 60;
+    this.video = videoElement;
     this.canvasId = canvasId;
     this.cap = new cv.VideoCapture(videoElement);
     this.frame = new cv.Mat(videoElement.height, videoElement.width, cv.CV_8UC4)
     this.gray = new cv.Mat();
+    //this.equalize = new cv.Mat();
     this.corners = new cv.Mat();
-    this.count = 0;
   }
 
   startTick(fps) {
@@ -19,17 +20,18 @@ class OpenCVVideo {
   }
 
   tick() {
-    this.count++;
     const begin = Date.now();
     this.cap.read(this.frame)
     cv.cvtColor(this.frame, this.gray, cv.COLOR_RGBA2GRAY);
-    cv.goodFeaturesToTrack(this.gray, this.corners, 2000, 0.01, 15);
-    console.log(this.corners);
-    cv.imshow(this.canvasId, this.gray);
-
-    if (this.count > 20) {
-      return;
+    //cv.equalizeHist(this.gray, this.equalize);
+    cv.goodFeaturesToTrack(this.gray, this.corners, 1500, 0.01, 15);
+    //cv.goodFeaturesToTrack(this.equalize, this.corners, 1500, 0.01, 15);
+    for (var i = 0; i < this.corners.rows; i++) {
+      var [x, y] = [this.corners.floatAt(i, 0), this.corners.floatAt(i, 1)];
+      cv.circle(this.frame, new cv.Point(x, y), 3, [255, 0, 0, 255], -1);
     }
+    cv.imshow(this.canvasId, this.frame);
+
     setTimeout(function (that) {
       that.tick();
     }, 1000 / this.fps - (Date.now() - begin), this)
